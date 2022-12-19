db239d2342f729178908d466fdc7fe84f2542d68
duongdinh24 <dvdinh24@gmail.com>
2022-12-19 23:20:55 +0700

    Done footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './layouts/Header'
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Header />
-    </>
+      <div className="content">Content</div>
+      <Footer />
+    </div>
   )
 }
 
